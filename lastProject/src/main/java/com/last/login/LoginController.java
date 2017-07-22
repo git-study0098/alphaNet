3cfb6c5c2f37dbcc8cd1c5a9e87ca23d2ad83137
jky142 <jky142@naver.com>
2017-07-22 11:37:06 +0900

    로그아웃 경로 수정

--- lastProject/src/main/java/com/last/login/LoginController.java
+++ lastProject/src/main/java/com/last/login/LoginController.java
@@ -1,11 +1,8 @@
 package com.last.login;
 
-import java.security.Principal;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
